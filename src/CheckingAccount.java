ddb0245f401aeb04f1f999a79dfd07b9cd5db358
AlecCSU <alexander.ross@csuglobal.edu>
2024-03-24 14:46:41 -0500

    Class Implementation
    - implemented functionality for all three classes
    - added 'ProjectScreenshots' folder

--- src/CheckingAccount.java
+++ src/CheckingAccount.java
@@ -1,3 +1,35 @@
-public class CheckingAccount {
-    
+public class CheckingAccount extends BankAccount {
+    private double interestRate;
+
+    // Constructor
+    public CheckingAccount(double interestRate) {
+        super(); // Initialize the superclass (BankAccount) part of this object
+        this.interestRate = interestRate;
+    }
+
+    // Overriding the withdrawal method to include overdraft functionality
+    @Override
+    public void withdrawal(double amount) {
+        if (amount > getBalance()) {
+            super.withdrawal(amount + 30); // Apply overdraft fee
+            System.out.println("Overdraft! A $30 fee has been charged.");
+        } else {
+            super.withdrawal(amount);
+        }
+    }
+
+    // Display account information including interest rate
+    public void displayAccount() {
+        super.accountSummary(); // Display superclass account information
+        System.out.println("Interest Rate: " + interestRate + "%");
+    }
+
+    // Getters and setters for interestRate
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
 }
--- src/TestAccounts.java
+++ src/TestAccounts.java
@@ -1,3 +1,17 @@
 public class TestAccounts {
-    
+    public static void main(String[] args) {
+        // Create a CheckingAccount object
+        CheckingAccount myAccount = new CheckingAccount(1.5);
+        myAccount.setFirstName("John");
+        myAccount.setLastName("Doe");
+        myAccount.setAccountID(12345);
+
+        // Test deposit and withdrawal
+        myAccount.deposit(100);
+        myAccount.withdrawal(50);
+        myAccount.withdrawal(60); // This should trigger an overdraft fee
+
+        // Display account summary
+        myAccount.displayAccount();
+    }
 }
